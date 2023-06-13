0ec7218f18c56adc47c4beedfc1d58c9935233b7
Lgluisdeveloper <132622538+Lgluisdeveloper@users.noreply.github.com>
2023-06-13 18:45:54 -0400

    master

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector(".nav__menu");
+let links = document.querySelector(".nav__links");
+
+console.log(menu);
+console.log(links);
+
+menu.addEvent;
+
+menu.addEventListener("click", () => {
+  links.classList.toggle("nav__link--show");
+});
